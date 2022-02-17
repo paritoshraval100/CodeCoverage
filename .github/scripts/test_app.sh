9b6eb0d94d3cf7f48d0c505cb7a4a9cb152c3657
your.user.name <paritosh.raval@infostretch.com>
2022-02-17 23:18:39 +0530

    added coverage

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -5,4 +5,5 @@ set -eo pipefail
 xcodebuild -project CodeCoverage.xcodeproj \
             -scheme CodeCoverage \
             -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 12 \
+            -enableCodeCoverage=YES
             clean test | xcpretty
