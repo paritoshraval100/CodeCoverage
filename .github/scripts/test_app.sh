0608575ec92724018e97fe1533469e58c2067ff9
your.user.name <paritosh.raval@infostretch.com>
2022-02-17 22:23:14 +0530

    changed script

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 xcodebuild -project CodeCoverage.xcodeproj \
-            -scheme CodeCoverage\ iOS \
+            -scheme CodeCoverage \
             -destination platform=iOS\ Simulator,OS=15.2,name=iPhone\ 12 \
             clean test | xcpretty
